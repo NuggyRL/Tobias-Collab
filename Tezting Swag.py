b54ae22ca9ae5f72616055537a0c024e13a4cd1a
Nuggy <karlarnesand123@gmail.com>
2021-02-02 21:33:54 +0100

    change

--- Tezting Swag.py
+++ Tezting Swag.py
@@ -1,4 +1,16 @@
 import time
 import random
 
-class Testing
+def testing():
+    print("Swagger")
+    x = 69
+    x += 1
+    if x < 100:
+        testing()
+    else:
+        break
+
+testing()
+
+# hvordan starter man her? no idea men ja vi må jo egt bare lage koden fordi vi kan paste den
+# ok nå pusher jeg changen
